68205d55266edbc947c8d892201d9fb22e148adb
Aasma Kanwal <aasmakanwal921@gmail.com>
2020-06-17 00:01:14 +0500

    start doing js 2nd assignment

---
+++ aap.js
@@ -0,0 +1,119 @@
+// ----- Chapter # 21-25 -----
+
+//  Task # 1
+
+// var firstName = prompt("Enter first name")
+// var lastName = prompt("Enter last name")
+// var fullName = firstName +" "+ lastName
+// document.write("Welcom "+ fullName)
+
+//  Task # 2
+
+// var favmobmad = prompt("Enter your favorite mobile model")
+// var g = favmobmad
+// for (i = 0 ; i = favmobmad.length ; i++){
+//     document.write("<b>My favorite phone is:</b> " + favmobmad)
+//     document.write("<br><b>Length of string is: </b>"+i)
+//     break
+// }
+
+//  Task # 3
+
+// var string = "Pakistani" 
+// document.write("<b>String: </b>"+ string + "<br>")  
+// for (i= 0 ; i < string.length ; i++){
+//     if (string[i] === "n"){
+//         document.write("The index number of 'n' is: " + i + "<br>")
+//     }
+// }
+//  Task # 4
+
+
+
+//  Task # 5
+// var string = "Pakistani" 
+// document.write("<b>String: </b>"+ string + "<br>") 
+// for (i= 0 ; i < string.length ; i++){
+//     if (i === 3){
+//         document.write("Character at index 3: " + string[i])
+//     }
+// }
+
+//  Task # 6
+//  Task # 7
+
+var string = "Hydarabad"
+
+//  Task # 8
+
+// var message = "Ali and Sami are best friends. They play cricket and football together."
+// var message1 = message.replace("and","&")
+// document.write(message1)
+
+//  Task # 9
+
+// var string = "472"
+// var tounumber = parseInt(472)
+// document.write("<br><b>Value:</b> " + string)
+// document.write("<br><b>Type:</b> String ")
+// document.write("<br><b>Value:</b> " + tounumber)
+// document.write("<br><b>Type:</b> Number ")
+
+//  Task # 10
+
+// var userinput = prompt("enter some text")
+// var casechange= userinput.toUpperCase()
+// document.write("User input: "+ userinput)
+// document.write("<br>Upper case: "+ casechange )
+
+//  Task # 11
+
+// var useinput2 = prompt("Enter your text")
+// var firstChar = useinput2.slice(0, 1);
+// var otherChars = useinput2.slice(1);
+// firstChar = firstChar.toUpperCase();
+// otherChars = otherChars.toLowerCase();
+// var cappedCity = firstChar + otherChars;
+// document.write(cappedCity)
+
+
+//  Task # 12
+
+// var num = 35.36
+// var tostring =(num*100).toString()
+// document.write("<b>Number: </b>"+ num)
+// document.write("<b><br>Result:</b> "+tostring )
+
+//  Task # 13
+//  Task # 14
+
+// var A =["cake", "apple pie", "cookies", "chips", "patties"]
+// var food = prompt("Welcom to ABC Bakery.What do you want to order sir/ma'am")
+// var flag = false
+// for (i= 0; i < food.length; i++){
+//     if (A[i] === food){
+//         document.write(food+" is available at index " + i)
+//         flag = true
+//         break   
+//     }
+// }
+// if (flag == false){
+//     document.write("We are sorry "+food+" is not available at our bakery")
+// }
+
+//  Task # 15
+//  Task # 16
+//  Task # 17
+//  Task # 18
+
+
+
+
+
+
+
+
+
+//  Task # 1
+//  Task # 
+//  Task # 
